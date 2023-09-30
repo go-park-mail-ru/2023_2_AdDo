de8900af62f31f5d98c654096ef565d03c0957c5
Kayetana <Ane4kaGr@mail.ru>
2023-10-01 02:32:56 +0300

    implement ability to add whole album

--- database/fill_db_script.py
+++ database/fill_db_script.py
@@ -39,18 +39,24 @@ server = conf["server_addr"]
 artist_name = conf["artist_name"]
 album_name = conf["album_name"]
 album_release = conf["album_release_date"]
-track_name = conf["track_name"]
+tracks = conf["tracks"]
 
-artist_avatar_url = server + "/images/avatars/artists/" + artist_name + ".jpg"
-album_url = server + "/audio/" + artist_name + "/albums/" + album_name
-track_url = server + "/audio/" + artist_name + "/albums/" + album_name + "/" + track_name
+artist_avatar_url = server + "/images/avatars/artists/" + artist_name.replace(" ", "_") + ".jpg"
+album_url = server + "/audio/" + artist_name.replace(" ", "_") + "/albums/" + album_name.replace(" ", "_")
+album_image = server + "/images/tracks/" + artist_name.replace(" ", "_") + "/albums/" + album_name.replace(" ", "_") + ".jpg"
 
-print(artist_avatar_url, "\n", album_url, "\n", track_url)
+# print(artist_avatar_url)
+# print(album_url)
+# print(album_image, "\n")
 
 
 with open("data_init.sql", "w") as file:
     file.write(create_artist_command(artist_name, artist_avatar_url) + '\n')
     file.write(create_album_command(artist_name, album_name, album_url, album_release) + '\n')
-    file.write(create_track_command(track_name, track_url, "music url here", album_name, artist_name) + '\n')
+
+    for track in tracks:
+        track_url = server + "/audio/" + artist_name.replace(" ", "_") + "/albums/" + album_name.replace(" ", "_") + "/" + track.replace(" ", "_") + ".mp3"
+        # print(track_url)
+        file.write(create_track_command(track, album_image, track_url, album_name, artist_name) + '\n')
 
 file.close()
