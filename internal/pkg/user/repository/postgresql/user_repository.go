bdb6a516bb2343b045f4e29b8812caad7dff27f8
dima <dima>
2023-10-01 17:02:58 +0300

    add tests for user repo, move md5 to utils, fix user repo

--- internal/pkg/user/repository/postgresql/user_repository.go
+++ internal/pkg/user/repository/postgresql/user_repository.go
@@ -1,9 +1,8 @@
 package user_repository
 
 import (
-	"crypto/md5"
 	"database/sql"
-	"encoding/hex"
+	"main/internal/pkg/common/utils"
 	"main/internal/pkg/user"
 )
 
@@ -16,28 +15,37 @@ func NewPostgres(db *sql.DB) *Postgres {
 }
 
 func (db *Postgres) Create(user user_domain.User) (uint64, error) {
-	var id uint64
-	hash := md5.Sum([]byte(user.Password))
-	hashString := hex.EncodeToString(hash[:])
-	err := db.Database.QueryRow("insert into profile (email, password, nickname, birth_date) values ($1, $2, $3, $4) returning id",
-		user.Email, hashString, user.Username, user.BirthDate).Scan(&id)
-	return id, err
+	var id int64
+
+	row, err := db.Database.Exec("insert into profile (email, password, nickname, birth_date) values ($1, $2, $3, $4) returning id",
+		user.Email, utils.GetMD5Sum(user.Password), user.Username, user.BirthDate)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err = row.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(id), nil
 }
 
 func (db *Postgres) GetById(id uint64) (user_domain.User, error) {
 	user := user_domain.User{Id: id}
-	err := db.Database.QueryRow("select email, nickname, birth_date, avatar_url from profile where id = $1", id).Scan(&user.Email, user.Username, user.BirthDate, user.Avatar)
+	err := db.Database.QueryRow("select email, nickname, birth_date, avatar_url from profile where id = $1", id).Scan(&user.Email, &user.Username, &user.BirthDate, &user.Avatar)
 	if err != nil {
-		return user, user_domain.ErrUserDoesNotExist
+		return user, err
 	}
 	return user, nil
 }
 
 func (db *Postgres) CheckEmailAndPassword(email, password string) (uint64, error) {
-	hash := md5.Sum([]byte(password))
-	hashString := hex.EncodeToString(hash[:])
-
 	var id uint64
-	err := db.Database.QueryRow("select id from profile where email = $1 and password = $2", email, hashString).Scan(&id)
-	return id, err
+	err := db.Database.QueryRow("select id from profile where email = $1 and password = $2", email, utils.GetMD5Sum(password)).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
