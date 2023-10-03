e612158bd7aacf51c2472dd9f5dcf13f3a08342b
Oleg <osperelygin@gmail.com>
2023-10-03 14:11:22 +0300

    fix json tags

--- internal/pkg/user/domain.go
+++ internal/pkg/user/domain.go
@@ -2,16 +2,17 @@ package user_domain
 
 import (
 	"errors"
+
 	"github.com/asaskevich/govalidator"
 )
 
 type User struct {
-	Id        uint64 `valid:"-" json:"id" example:"1"`
-	Username  string `valid:"length(2|30), required" json:"username" example:"john"`
-	Email     string `valid:"length(1|30), email, required" json:"email" example:"example@gmail.com"`
-	Password  string `valid:"length(6|30), required" json:"password" example:"password"`
-	BirthDate string `valid:"required" json:"date" example:"2000-01-01"`
-	Avatar    string `valid:"url_optional" json:"avatar" example:"http://test/image/1.jpg,http://test/image/2.jpg"`
+	Id        uint64 `valid:"-" json:"Id" example:"1"`
+	Username  string `valid:"length(2|30), required" json:"Username" example:"john"`
+	Email     string `valid:"length(1|30), email, required" json:"Email" example:"example@gmail.com"`
+	Password  string `valid:"length(6|30), required" json:"Password" example:"password"`
+	BirthDate string `valid:"required" json:"BirthDate" example:"2000-01-01"`
+	Avatar    string `valid:"url_optional" json:"Avatar" example:"http://test/image/1.jpg,http://test/image/2.jpg"`
 }
 
 func (u *User) Validate() error {
@@ -23,7 +24,7 @@ func (u *User) Validate() error {
 }
 
 type ResponseId struct {
-	Id uint64 `json:"id" example:"1"`
+	Id uint64 `json:"Id" example:"1"`
 }
 
 type UseCase interface {
