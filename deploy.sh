02efb5ec351b7b1ad80bfd9d029b7c370bdef6af
dima <dima>
2023-10-07 21:03:53 +0300

    add Makefile with commands

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-# shellcheck disable=SC2046
-docker stop $(docker ps -a -q);
-docker rm $(docker ps -a -q);
-docker rmi $(docker images -q);
-docker network rm $(docker network ls -q);
-cd database && python3 fill_db_script.py;
-cd .. && docker compose up;
