b4366c7d504f04ce17c3c74a2faad0e00e2572b0
Kayetana <Ane4kaGr@mail.ru>
2023-10-13 12:57:36 +0300

    remove some tests and unused functions

--- internal/pkg/track/repository/postgresql/track_repository.go
+++ internal/pkg/track/repository/postgresql/track_repository.go
@@ -14,27 +14,6 @@ func NewPostgres(pool postgres.PgxIFace) *Postgres {
 	return &Postgres{Pool: pool}
 }
 
-//func (db *Postgres) getTracks(query string, limit uint32) ([]track.Response, error) {
-//	rows, err := db.Pool.Query(context.Background(), query, limit)
-//
-//	if err != nil {
-//		return nil, err
-//	}
-//	defer rows.Close()
-//
-//	result := make([]track.Response, 0)
-//	for rows.Next() {
-//		var t track.Response
-//		err = rows.Scan(&t.Id, &t.Name, &t.Preview, &t.Content)
-//		if err != nil {
-//			return nil, err
-//		}
-//
-//		result = append(result, t)
-//	}
-//	return result, nil
-//}
-
 //func (db *Postgres) GetAll() ([]track.Response, error) {
 //	query := "select id, name, preview, content from track"
 //	return db.getTracks(query, 0)
@@ -49,50 +28,34 @@ func NewPostgres(pool postgres.PgxIFace) *Postgres {
 //	query := "select id, name, preview, content from track order by release_date desc limit $1"
 //	return db.getTracks(query, limit)
 //}
-//
-//func (db *Postgres) GetByPlaylist(playlistId uint64) ([]track.Response, error) {
-//	query := "select track_id from playlist_track where playlist_id = $1"
-//	return db.getTracksById(query, playlistId)
-//}
 
-func (db *Postgres) getTracksById(query string, id uint64) ([]track.Response, error) {
-	rows, err := db.Pool.Query(context.Background(), query, id)
+func (db *Postgres) getWithQuery(ctx context.Context, query string, args ...any) ([]track.Response, error) {
+	rows, err := db.Pool.Query(ctx, query, args...)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	tracks := make([]track.Response, 0)
+	result := make([]track.Response, 0)
 	for rows.Next() {
-		var trackId int
-		err = rows.Scan(&trackId)
+		var t track.Response
+		err := rows.Scan(&t.Id, &t.Name, &t.Preview, &t.Content)
 		if err != nil {
 			return nil, err
 		}
-		t, err := db.getById(uint64(trackId))
-		if err != nil {
-			return nil, err
-		}
-		tracks = append(tracks, t)
+		result = append(result, t)
 	}
-	return tracks, nil
-}
-
-func (db *Postgres) getById(trackId uint64) (track.Response, error) {
-	query := "select id, name, preview, content from track where id = $1"
-
-	var t track.Response
-	err := db.Pool.QueryRow(context.Background(), query, trackId).Scan(&t.Id, &t.Name, &t.Preview, &t.Content)
-	if err != nil {
-		var empty track.Response
-		return empty, err
-	}
-	return t, nil
+	return result, nil
 }
 
 func (db *Postgres) GetByAlbum(albumId uint64) ([]track.Response, error) {
-	query := "select track_id from album_track where album_id = $1"
-	return db.getTracksById(query, albumId)
+	query := "select track.id, name, preview, content from track join album_track on track.id = album_track.track_id where album_track.album_id = $1"
+	return db.getWithQuery(context.Background(), query, albumId)
+}
+
+func (db *Postgres) GetByArtist(artistId uint64) ([]track.Response, error) {
+	query := "select track.id, name, preview, content from track join artist_track on track.id = artist_track.track_id where artist_track.artist_id = $1"
+	return db.getWithQuery(context.Background(), query, artistId)
 }
 
 func (db *Postgres) CreateLike(userId, trackId uint64) error {
@@ -113,9 +76,4 @@ func (db *Postgres) AddListen(trackId uint64) error {
 	}
 
 	return nil
-}
-
-func (db *Postgres) GetByArtist(artistId uint64) ([]track.Response, error) {
-	query := "select track_id from artist_track where artist_id = $1"
-	return db.getTracksById(query, artistId)
 }
