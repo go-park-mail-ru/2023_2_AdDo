7dd16edb857f494f7b53038387e067079d02f016
dima <dima>
2023-10-18 21:54:40 +0300

    add logging for artist

--- internal/pkg/artist/delivery/http/artist_handler.go
+++ internal/pkg/artist/delivery/http/artist_handler.go
@@ -1,24 +1,26 @@
 package artist_delivery
 
 import (
+	"github.com/sirupsen/logrus"
 	"main/internal/pkg/artist"
 	common_handler "main/internal/pkg/common/handler"
 	"main/internal/pkg/common/response"
-	"main/internal/pkg/session"
+	"main/internal/pkg/common/utils"
 	"net/http"
 	"strconv"
 
-	"github.com/gorilla/csrf"
 	"github.com/gorilla/mux"
 )
 
 type ArtistHandler struct {
 	ArtistUseCase artist.UseCase
+	logger        *logrus.Logger
 }
 
-func NewHandler(artistUseCase artist.UseCase) ArtistHandler {
+func NewHandler(artistUseCase artist.UseCase, logger *logrus.Logger) ArtistHandler {
 	return ArtistHandler{
 		ArtistUseCase: artistUseCase,
+		logger:        logger,
 	}
 }
 
@@ -33,22 +35,27 @@ func NewHandler(artistUseCase artist.UseCase) ArtistHandler {
 //	@Failure		500	{string}	errMsg
 //	@Router			/artist/{id} [get]
 func (handler *ArtistHandler) ArtistInfo(w http.ResponseWriter, r *http.Request) error {
-	w.Header().Set(session.XCsrfToken, csrf.Token(r))
+	handler.logger.WithFields(logrus.Fields{
+		"request_id": utils.GenReqId(r.RequestURI + r.Method),
+	}).Infoln("ArtistInfo Handler entered")
 
 	artistId, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		return common_handler.StatusError{Code: http.StatusBadRequest, Err: err}
 	}
+	handler.logger.Infoln("Parsed artistId from Vars")
 
 	artistInfo, err := handler.ArtistUseCase.GetArtistInfo(uint64(artistId))
 	if err != nil {
-		return common_handler.StatusError{Code: http.StatusInternalServerError, Err: err}
+		return common_handler.StatusError{Code: http.StatusNotFound, Err: err}
 	}
+	handler.logger.Infoln("Got artist from use case")
 
 	err = response.RenderJSON(w, artistInfo)
 	if err != nil {
 		return common_handler.StatusError{Code: http.StatusInternalServerError, Err: err}
 	}
+	handler.logger.Infoln("response  formed")
 
 	return nil
 }
