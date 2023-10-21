fca3449584c1bce5be8794eb3a045e98ba306d8f
dima <dima>
2023-10-21 16:20:37 +0300

    add micros for session model

---
+++ internal/microservices/session/service/server/session_service_server.go
@@ -0,0 +1,38 @@
+package grpc_session_server
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	pb "main/internal/microservices/session/proto"
+	"main/internal/pkg/session"
+)
+
+type SessionManager struct {
+	repoSession session.Repository
+	logger      *logrus.Logger
+	pb.UnimplementedSessionServiceServer
+}
+
+func (sm *SessionManager) CheckSession(ctx context.Context, in *pb.SessionId) (*pb.Status, error) {
+	sm.logger.Infoln("Session Micros CheckSession entered")
+
+	_, err := sm.repoSession.Get(in.GetSessionId())
+	if err != nil {
+		return &pb.Status{IsOk: false}, err
+	}
+	sm.logger.Infoln("Session id matched with an database one")
+
+	return &pb.Status{IsOk: true}, nil
+}
+
+func (sm *SessionManager) GetUserId(ctx context.Context, in *pb.SessionId) (*pb.UserId, error) {
+	sm.logger.Infoln("Session Micros GetUserId entered")
+
+	userId, err := sm.repoSession.Get(in.GetSessionId())
+	if err != nil {
+		return nil, err
+	}
+	sm.logger.Infoln("Got user id: ", userId)
+
+	return &pb.UserId{UserId: userId}, nil
+}
