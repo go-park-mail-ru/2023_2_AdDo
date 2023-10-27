f32e61c6bcc462c7a097df510ae9f9ae94e6f9b0
Oleg <osperelygin@gmail.com>
2023-10-27 23:35:19 +0300

    fix pipeline

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 echo "Preparing environments.."
-docker-compose -f deployments/test/docker-compose.yml up -d
-echo "Preparing successful done"
-
-echo "Running tests with empty database..."
-python3 test/testsuite/sign_up_test.py
-python3 test/testsuite/music_test.py
-python3 test/testsuite/me_test.py
-python3 test/testsuite/logout_test.py
-python3 test/testsuite/login_test.py
-python3 test/testsuite/listen_test.py
-python3 test/testsuite/auth_test.py
+docker compose -f deployments/test/docker-compose.yml up -d
 if [ $? -ne 0 ]; then
-    echo "Error integration tests"
+    echo "Error creating environment"
+    docker compose -f deployments/test/docker-compose.yml down
     exit 1
 fi
 
+echo "Preparing successful done"
+
+echo "Running tests with empty database..."
+for test in test/testsuite/*; do
+    python3 $test
+    if [ $? -ne 0 ]; then
+        echo "Error integration tests"
+        docker compose -f deployments/test/docker-compose.yml down
+        exit 1
+    fi
+done
+
 echo "Tests passed"
 
-docker-compose -f deployments/test/docker-compose.yml down
+docker compose -f deployments/test/docker-compose.yml down
 if [ $? -ne 0 ]; then
     echo "Error delete test environment"
     exit 1
