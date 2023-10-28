b88ebf2a808b9379d0052024fbfea38c31b6c91e
dima <dima>
2023-10-28 03:56:05 +0300

    fix pipeline

--- scripts/docker-services-build.sh
+++ scripts/docker-services-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo 'Building musicon backend...' 	
+echo 'Building musicon backend...'
+docker build -t 2023_2_addo-musicon/user -f build/package/user/Dockerfile .
 docker compose -f deployments/prod/docker-compose.yml build
 echo 'Musicon backend built successful'
