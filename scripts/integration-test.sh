9191d14a7538f8069dc2b6e40a48d52fb7b778d4
dima <dima>
2023-10-28 04:05:48 +0300

    fix pipeline

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Preparing environments.."
-docker build -t 2023_2_addo-musicon/user -f build/package/user/Dockerfile .
+docker build -t 2023_2_addo-musicon/base -f build/package/base/Dockerfile .
 docker compose -f deployments/test/docker-compose.yml up -d
 if [ $? -ne 0 ]; then
     echo "Error creating environment"
