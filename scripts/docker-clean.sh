0e32ef5139bc0da61cd85cb0cedc1dc4b2f80be3
dmitry <edp032017@yandex.ru>
2023-11-04 02:36:54 +0300

    fix deployment makefile

--- scripts/docker-clean.sh
+++ scripts/docker-clean.sh
@@ -6,4 +6,14 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# shellcheck disable=SC2046
+docker stop $(docker ps -a -q) -f
+# shellcheck disable=SC2046
+docker rm $(docker ps -a -q) -f
+# shellcheck disable=SC2046
+docker rmi $(docker images -q) -f
+# shellcheck disable=SC2046
+docker volume rm $(docker volume ls -q) -f
+# shellcheck disable=SC2046
+docker network rm $(docker network ls -q) -f
 echo "Successful cleaning"
