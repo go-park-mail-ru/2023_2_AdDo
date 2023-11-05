bc02a9d72390a82155970c6ee1bbfdf2d6695e9b
Kayetana <Ane4kaGr@mail.ru>
2023-11-06 00:00:29 +0300

    add tests for image, playlist, session and user service

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -7,8 +7,10 @@ mockgen -source=internal/pkg/track/domain.go -destination=test/mocks/track/track
 mockgen -source=internal/pkg/artist/domain.go -destination=test/mocks/artist/artist_mock.go -package=artist_mock;
 mockgen -source=internal/pkg/album/domain.go -destination=test/mocks/album/album_mock.go -package=album_mock;
 mockgen -source=internal/pkg/image/domain.go -destination=test/mocks/avatar/avatar_mock.go -package=avatar_mock;
+mockgen -source=internal/pkg/playlist/domain.go -destination=test/mocks/playlist/playlist_mock.go -package=playlist_mock;
+
 echo "Running unit tests..."
 go test -coverprofile=all_files -coverpkg=./... ./...
-cat all_files | grep -v "cmd" | grep -v "test" | grep -v "init" > testing_files
+cat all_files | grep -v "cmd" | grep -v "test" | grep -v "init" | grep -v "/.*proto.*" > testing_files
 go tool cover -func=testing_files
 rm testing_files all_files
