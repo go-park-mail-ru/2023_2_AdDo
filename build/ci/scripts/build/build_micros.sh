29ec224e21a800b99984fcf48f3fc4f609cff9bf
Oleg <osperelygin@gmail.com>
2023-11-06 14:55:19 +0300

    fix ci scripts

--- build/ci/scripts/build/build_micros.sh
+++ build/ci/scripts/build/build_micros.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d "/tmp/images/" ]; then
+    mkdir /tmp/images/
+fi
+
 for dockerfile in build/package/micros/*/Dockerfile; do
     image_name=$(basename $(dirname $dockerfile))
     docker build -t $image_name -f $dockerfile --output type=tar,dest=/tmp/images/$image_name.tar .
