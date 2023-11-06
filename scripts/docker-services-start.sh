f9a94b0db8ccc1ede45827ecd508b98dd59dbbf0
Oleg <osperelygin@gmail.com>
2023-11-06 12:03:28 +0300

    fix makefile

--- scripts/docker-services-start.sh
+++ scripts/docker-services-start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Starting containers..."
-docker compose -f deployments/prod/docker-compose.yml up -d
 
+docker compose -f deployments/prod/docker-compose.yml up -d
 if [ $? -ne 0 ]; then
     echo "Error while starting containers"
     exit 1
