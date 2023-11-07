c0008b01648be87b9ccd7dfafc40caa0c1f89a04
Oleg <osperelygin@gmail.com>
2023-11-08 00:08:18 +0300

    fix image tags

--- build/ci/scripts/load/load_micros.sh
+++ build/ci/scripts/load/load_micros.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+image_tag=$1
+registry=registry.musicon.space
 for arch in /tmp/images/*.tar; do
-    cat $arch | docker import - $(basename $arch .tar):latest
+    image_name=$(basename $arch .tar)
+    cat $arch | docker import - $registry/$image_name:$image_tag
     if [ $? -ne "0" ]; then
-        echo "Error while loading $arch"
+        echo "Error while loading $registry/$image_name:$image_tag"
         exit 1
     fi
 done
