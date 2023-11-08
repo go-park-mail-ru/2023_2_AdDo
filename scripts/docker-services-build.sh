5d78da64007e86cbe0fbdcf5dbc5661f89d33980
dmitry <edp032017@yandex.ru>
2023-11-08 15:23:34 +0300

    fix time conv and script for build

--- scripts/docker-services-build.sh
+++ scripts/docker-services-build.sh
@@ -1,5 +1,5 @@
 for dockerfile in build/package/micros/*/Dockerfile; do
-    image_name=registry.musicon.space/$(basename $(dirname $dockerfile)):test
+    image_name=registry.musicon.space/$(basename $(dirname $dockerfile))
     docker build -t $image_name -f $dockerfile .
     if [ $? -ne "0" ]; then
         echo "Error while building $image_name image"
