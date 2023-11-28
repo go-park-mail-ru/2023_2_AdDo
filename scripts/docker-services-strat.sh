125d23bd82b4ce7411b80b207157d9ca11fcd41d
dmitry <edp032017@yandex.ru>
2023-11-29 01:28:36 +0300

    debug pipeline

--- scripts/docker-services-strat.sh
+++ scripts/docker-services-strat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose -f deployments/test/docker-compose.yml up -d
+docker compose -f deployments/test/docker-compose.yml up
 if [ $? -ne 0 ]; then
     echo "Error execute "docker compose -f deployments/test/docker-compose.yml up -d""
     docker compose -f deployments/test/docker-compose.yml down
