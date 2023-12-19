e4a70211d94297acf9ea32587a1bb7d94648303b
Kayetana <Ane4kaGr@mail.ru>
2023-12-19 16:20:29 +0300

    fix integration tests

--- test/testsuite/utils.py
+++ test/testsuite/utils.py
@@ -5,6 +5,7 @@ import hashlib
 url = 'http://localhost:8888/api/v1'
 ws_url = 'ws://localhost:8888/api/v1'
 
+
 def gen_random_valid_register_data():
     username = names.get_full_name().replace(' ', '').lower()
     email = username + '@mail.ru'
@@ -29,14 +30,13 @@ def gen_random_valid_update_data():
 
 
 def get_csrf_headers_and_cookies():
-    response = requests.get(url + '/auth')
+    response = requests.get(url + '/get_csrf')
     cookies = {
         'X-Csrf-Token': response.cookies['X-Csrf-Token']
     }
     headers = {
         'X-Csrf-Token': response.headers['X-Csrf-Token']
     }
-
     return headers, cookies
 
 
