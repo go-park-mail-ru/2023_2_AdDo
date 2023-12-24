2ffa129870f32a0ee137d318cc9481897834e94d
Kayetana <Ane4kaGr@mail.ru>
2023-12-24 15:00:11 +0300

    fix script for setting is_single field

--- db/set_is_single.py
+++ db/set_is_single.py
@@ -2,10 +2,15 @@ import requests
 
 # url = 'http://localhost:8888/api/v1'
 url = "https://musicon.space/api/v1"
+albumsNum = 3200
 
-albumsNum = 1300
+# UPDATE album SET is_single = true WHERE id IN (1, 2, 3);
 
 with open('set_is_single.sql', "w") as f:
+    f.write("UPDATE album SET is_single = true WHERE id IN (")
+
+    first_id_is_written = False
+
     for i in range(1, albumsNum):
         print('url:', url + '/album/' + str(i))
         r = requests.get(url + '/album/' + str(i))
@@ -19,6 +24,14 @@ with open('set_is_single.sql', "w") as f:
 
         if len(tracks) == 1:
             print('Single')
-            f.write('update album set is_single = true where id = {};\n'.format(i))
+
+            if first_id_is_written:
+                f.write(', ')
+            f.write(str(i))
+
+            if not first_id_is_written:
+                first_id_is_written = True
 
         print('\n')
+
+    f.write(');')
